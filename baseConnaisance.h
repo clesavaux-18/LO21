f52cbf8f9e556e6e244044e95d653d7e0be98107
clesavaux-18 <clesavaux@gmail.com>
2025-12-23 16:32:43 +0100

    Update baseConnaisance.h

--- baseConnaisance.h
+++ baseConnaisance.h
@@ -1,1 +1,20 @@
+#ifndef __LISTE_H__ 
+#define __LISTE_H__
 
+#include <stdlib.h> 
+#include <stdio.h>
+#include "regle.h"
+
+typedef struct elemBC {
+	 regle connaisance;
+	 struct elem *next;
+} elementBC;
+
+typedef elementBC * BaseConnaisance;
+//fonction1
+//fonction2
+//fonction3
+//...
+
+
+#endif 
