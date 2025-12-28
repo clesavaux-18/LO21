106dafe7687fccdeaac84a28d3ad6eee63e4719d
clesavaux-18 <clesavaux@gmail.com>
2025-12-28 15:16:11 +0100

    Update BaseConnaissance.c

--- BaseConnaissance.c
+++ BaseConnaissance.c
@@ -5,14 +5,15 @@
 
 //lise chainée<regeles>:ajout en queue:créer base de connaisance vide/accéder a la règle en tete
 BaseConnaissance creerBaseConnaissanceVide() {
-    return NULL;
+    return NULL; // retourne un pointeur NULL
 }
 
 /*-----------------------------------------/VERIFICATIONS/---------------------------------------------------------------------------------------------------*/
 
 //prend en paramètre une base de connaisace BC.elle renvoie 1 si oui et 0 si non.
 bool estVideBaseConnaissance(BaseConnaissance BC) {
-    elementBC *p = BC;
+
+     //verifie si BC pointe vers NULL
     if (BC == NULL) {
         return 1;
     }return 0;
@@ -22,11 +23,17 @@ bool estVideBaseConnaissance(BaseConnaissance BC) {
 
 //prend en paramètre une base de connaisance BC et un pointeur vers une regle e .Le programe retourne la BC avec la règle e ajouté a cellle ci 
 BaseConnaissance insererQueueBaseConnaissance(BaseConnaissance BC, regle *e) {
-    elementBC *new = (elementBC *)malloc(sizeof(elementBC));
+    
+    //aloue un nouvel element avec notre  e en valeur et NULL en next en memoire avec malloc
+    elementBC *new = (elementBC *)malloc(sizeof(elementBC)); 
     new->connaissance = e;
     new->next = NULL;
+    
+    //verifie si la base est vide 
     if(estVideBaseConnaissance(BC)) {
         return new;
+        
+    //si elle est non vide parcoure jusqua la fin et redefinit le pointeur next du dernier element vers notre nouveau.
     }else {
         elementBC *p = BC;
         while (p->next != NULL) {
@@ -39,6 +46,8 @@ BaseConnaissance insererQueueBaseConnaissance(BaseConnaissance BC, regle *e) {
 
 //prend en paramètre un une base de connaisance BC et un pointeur vers une règle e. La fonction retourne la base avec la regle e enlevé si elle etait presente.
 BaseConnaissance supprimerUNEBaseConnaissance(BaseConnaissance BC, regle *e) {
+
+    //verifie si BC est NULL
     if (estVideBaseConnaissance(BC)) {
         return NULL;
     }
@@ -52,13 +61,17 @@ BaseConnaissance supprimerUNEBaseConnaissance(BaseConnaissance BC, regle *e) {
     }
 
     // Cas 2 : Suppression dans le reste de la liste
-    elementBC *p = BC;
+    
+    elementBC *p = BC;//création d'un pointeur de parcour 
+    //parcour tant que l'on est pas au bout ou que ça n'est pas notre e.
     while (p->next != NULL && p->next->connaissance != e) {
         p = p->next;
     }
-
+    // si on est au bout on n'aurat touché a rien et on retourne la base 
     if (p->next == NULL) {
         return BC; // Pas trouvé
+
+    //si c'est trouvé on retire et on retourne la base
     } else {
         elementBC *tmp = p->next;
         p->next = tmp->next;
@@ -70,6 +83,7 @@ BaseConnaissance supprimerUNEBaseConnaissance(BaseConnaissance BC, regle *e) {
 //prend en paramètre une base de connaisance BC et retourne aune base vide après avoir supprimé tout les éléments.
 BaseConnaissance supprimertoutBaseConnaissance(BaseConnaissance BC) {
     elementBC *p;
+    //tant que BC est non vide on parcourt et vide 
     while (!estVideBaseConnaissance(BC)) {
         p=BC;
         BC=BC-> next;
