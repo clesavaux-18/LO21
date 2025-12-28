0bf5a6ff0488c75efa214038ac69e6f0dd823d4e
clesavaux-18 <clesavaux@gmail.com>
2025-12-28 14:47:43 +0100

    Update BaseFait.c

--- BaseFait.c
+++ BaseFait.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "BaseFait.h"
 
-
+//prend en paramètre une liste de fait l(base de fait) et ajoute en queue un fait e chaine de charactère. la fonction retourne la listef ainsi modifié
 listf insererQueue(listf l, char e[1000]) {
     elementf *new = (elementf *)malloc(sizeof(elementf));
     strncpy(new->value, e,1000);
@@ -19,6 +19,7 @@ listf insererQueue(listf l, char e[1000]) {
     return l;
 }
 
+//prend en paramètre une liste l et renvoie une liste vide après avoir suprimé chaque élément 
 listf supprimertout(listf l) {
     elementf *p;
     while (!siVide(l)) {
@@ -29,6 +30,7 @@ listf supprimertout(listf l) {
     return l;
 }
 
+//prend en paramètre une liste l et un fait e et retourne l auquel on a enlevé le fait e si il etait présent
 listf supprimer_element(listf l, char e[1000]) {
     if (siVide(l)) {
         return NULL;
@@ -59,10 +61,13 @@ listf insererTete (listf l, char e[1000]) {
     return new;
 }
 
+//prend en paramètre une une liste l et vérifie si elle est vide. la fonction retourne une 1 si oui et un 0 si non 
 bool siVide(listf l) {
     // Si l est NULL, c'est vide (Vrai), sinon Faux.
     return (l == NULL);
 }
+
+// cette fonction prend en paramètre une liste l et un fait e.Elle retourne 1(true) si e est present dans l et 0(false) si non
 bool recherche(listf l, char *e) {
     // Cas de base trivial
     if (l == NULL) {
@@ -76,4 +81,5 @@ bool recherche(listf l, char *e) {
         // On continue de chercher dans la suite
         return recherche(l->next, e);
     }
+
 }
